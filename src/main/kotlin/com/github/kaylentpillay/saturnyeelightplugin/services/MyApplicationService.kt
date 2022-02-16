1f4d31d79add475db177c82e3609c87708772ff1
GitHub Action <action@github.com>
2022-02-16 16:43:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kaylentpillay/saturnyeelightplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kaylentpillay.saturnyeelightplugin.services
+
+import com.github.kaylentpillay.saturnyeelightplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kaylentpillay/saturnyeelightplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kaylentpillay/saturnyeelightplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kaylentpillay.saturnyeelightplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kaylentpillay.saturnyeelightplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
